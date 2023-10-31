fb2d1dee34187d1bb60aef314afe474442dae0bf
auraShy <davidcamunezrodriguez@gmail.com>
2023-10-31 10:37:24 +0100

    Changes in MainMundo from mdAlturasV2

--- src/main/java/org/uma/mbd/mdAlturasV2/MainMundo.java
+++ src/main/java/org/uma/mbd/mdAlturasV2/MainMundo.java
@@ -1,7 +1,7 @@
 package org.uma.mbd.mdAlturasV2;
 
-import org.uma.mbd.mdAlturasV2.alturas.EnContinente;
 import org.uma.mbd.mdAlturasV2.alturas.MenoresQue;
+import org.uma.mbd.mdAlturasV2.alturas.EnContinente;
 import org.uma.mbd.mdAlturasV2.alturas.Mundo;
 import org.uma.mbd.mdAlturasV2.alturas.Pais;
 
@@ -15,31 +15,55 @@ public class MainMundo {
         Mundo paises = new Mundo();
         paises.leePaises("recursos/mdAlturas/alturas.txt");
 
-        Comparator<Pais> ordenAlt = Comparator.comparingDouble(Pais::altura);
-        Set<Pais> paisesOrdenAlt = new TreeSet<>(ordenAlt);
-        paisesOrdenAlt.addAll(paises.getPaises());
+        //APARTADO A
+        Comparator<Pais> ordenAltura = Comparator.comparingDouble(Pais::altura);
+        Set<Pais> paisesOrdenAlt = new TreeSet<>(ordenAltura);
+        //paisesOrdenAlt.addAll(paises.getPaises());
         //paisesOrdenAlt.forEach(System.out::println);
 
-        Comparator<Pais> ordenPaisAlf = Comparator.comparing(Pais::nombre);
-        Set<Pais> paisesOrdenAlf = new TreeSet<>(ordenPaisAlf);
-        paisesOrdenAlf.addAll(paises.getPaises());
+        //APARTADO B
+        Comparator<Pais> ordenAlfabetico = Comparator.comparing(Pais::nombre);
+        Set<Pais> paisesOrdenAlf = new TreeSet<>(ordenAlfabetico);
+        //paisesOrdenAlf.addAll(paises.getPaises());
         //paisesOrdenAlf.forEach(System.out::println);
 
+        //APARTADO C
         Comparator<Pais> ordenContinente = Comparator.comparing(Pais::continente);
-        Comparator<Pais> ordenContAlf = ordenContinente.thenComparing(ordenPaisAlf);
+        Comparator<Pais> ordenContAlf = ordenContinente.thenComparing(ordenAlfabetico);
         Set<Pais> paisesContAlf = new TreeSet<>(ordenContAlf);
-        paisesContAlf.addAll(paises.getPaises());
+        //paisesContAlf.addAll(paises.getPaises());
         //paisesContAlf.forEach(System.out::println);
 
-        Comparator<Pais> ordenContAlfRev = ordenContinente.thenComparing(ordenPaisAlf.reversed());
-        Set<Pais> paisesContAlfRev = new TreeSet<>(ordenContAlfRev);
-        paisesContAlfRev.addAll(paises.getPaises());
+        //APARTADO D
+        Comparator<Pais> ordenContAlfRever = ordenContinente.thenComparing(ordenAlfabetico.reversed());
+        Set<Pais> paisesContAlfRever = new TreeSet<>(ordenContAlfRever);
+        //paisesContAlfRever.addAll(paises.getPaises());
         //paisesContAlfRev.forEach(System.out::println);
 
+        //APARTADO E
         Comparator<Pais> ordenContNat = ordenContinente.thenComparing(Comparator.naturalOrder());
         Set<Pais> paisesContNat = new TreeSet<>(ordenContNat);
-        paisesContNat.addAll(paises.getPaises());
+        //paisesContNat.addAll(paises.getPaises());
+        //paisesContNat.forEach(System.out::println);
+
+        for (Pais pais : paises.selecciona(new MenoresQue(1.70))) {
+            paisesOrdenAlt.add(pais);
+            paisesOrdenAlf.add(pais);
+            paisesContAlf.add(pais);
+            paisesContAlfRever.add(pais);
+            paisesContNat.add(pais);
+        }
+        System.out.println("***********ORDEN POR ALTURA DE MENOR A MAYOR***********");
+        paisesOrdenAlt.forEach(System.out::println);
+        System.out.println("***********ORDEN ALFABETICO DE LOS PAISES***********");
+        paisesOrdenAlf.forEach(System.out::println);
+        System.out.println("***********ORDEN POR CONTINENTE Y A IGUALDAD, ALFABETICAMENTE***********");
+        paisesContAlf.forEach(System.out::println);
+        System.out.println("***********ORDEN POR CONTINENTE Y A IGUALDAD, ALFABETICAMENTE INVERSO***********");
+        paisesContAlfRever.forEach(System.out::println);
+        System.out.println("***********ORDEN POR CONTINENTE Y A IGUALDAD, ORDEN NATURAL***********");
         paisesContNat.forEach(System.out::println);
+
 
 
 
